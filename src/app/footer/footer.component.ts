164fe25665e18ba16c2c324c04a38a527a0e6cf9
Silvio Lampi <silvio.lampignano@skyit.it>
2023-10-10 13:38:30 +0200

    J-query library commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  scrollPosition: number;
+  windowHeight: number;
+  documentHeight: number;
 
+  constructor() {
+    this.scrollPosition = 0;
+    this.windowHeight = 0;
+    this.documentHeight = 0;
+
+    // Inoltre, assicurati di inizializzare queste variabili all'interno del costruttore
+    // quando il componente viene creato per la prima volta.
+    this.updateScrollInfo();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.updateScrollInfo();
+
+    if (this.scrollPosition + this.windowHeight >= this.documentHeight - 50) {
+      $('#site-footer').removeClass('hidden-footer').addClass('visible-footer');
+    } else {
+      $('#site-footer').removeClass('visible-footer').addClass('hidden-footer');
+    }
+  }
+
+  private updateScrollInfo() {
+    this.scrollPosition = $(window).scrollTop() || 0;
+    this.windowHeight = $(window).height() || 0;
+    this.documentHeight = $(document).height() || 0;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare var $: any;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  currentUrl: string = ''; // Inizializza currentUrl
 
+  tabContainerHeight: number = 0;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.url;
+        this.onRouteChange();
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.tabContainerHeight = $('.et-hero-tabs').height();
+    this.onRouteChange();
+  }
+
+  ngAfterViewInit() {
+    this.setupScrollSpy();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    this.onRouteChange();
+  }
+
+  onRouteChange() {
+    const self = this;
+    const element = $(`a[href='${this.currentUrl}']`);
+    if (element) {
+      const scrollTop = element.offset()?.top - this.tabContainerHeight + 1;
+      const offset = $('.et-hero-tabs').offset()?.top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+      if (offset !== undefined && $(window).scrollTop() > offset) {
+        element.parent().removeClass('et-hero-tab-active');
+      } else if (scrollTop !== undefined && $(window).scrollTop() >= scrollTop) {
+        element.parent().addClass('et-hero-tab-active');
+        element.parent().siblings().removeClass('et-hero-tab-active');
+      }
+    }
+  }
+
+  setupScrollSpy() {
+    $('a.scrollable').on('click', (event: Event) => { // Utilizza una freccia lambda qui
+      const id = $(event.currentTarget).attr('href'); // Utilizza event.currentTarget
+      event.preventDefault();
+      const target = $(id);
+      const offsetTop = target.offset()?.top;
+      const offsetBottom = offsetTop !== undefined && target ? offsetTop + (target[0] as HTMLElement).clientHeight - this.tabContainerHeight : undefined;
+
+      if (offsetTop !== undefined && offsetBottom !== undefined && $(window).scrollTop() >= offsetTop && $(window).scrollTop() < offsetBottom) {
+        const newCurrentId = id;
+        this.router.navigateByUrl(newCurrentId);
+      }
+    });
+  }
 }
