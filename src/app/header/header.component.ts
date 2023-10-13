02fb38b02292ffdea1acc7b7e2bba3a9f291def2
Silvio Lampi <silvio.lampignano@skyit.it>
2023-10-13 22:35:32 +0200

    assets needing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,62 +5,62 @@ import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  isSticky: boolean = false;
-  isHidden: boolean = false;
-  imagesLoaded = false;
+export class HeaderComponent  {
+  // isSticky: boolean = false;
+  // isHidden: boolean = false;
+  // imagesLoaded = false;
 
-  @ViewChild('etHeroTabsContainer')
-  etHeroTabsContainer!: ElementRef;
+  // @ViewChild('etHeroTabsContainer')
+  // etHeroTabsContainer!: ElementRef;
 
-  isMobileView = false; // Aggiungi questa variabile per gestire la vista mobile
+  // isMobileView = false; // Aggiungi questa variabile per gestire la vista mobile
 
-  images = [
-    {
-      src: 'assets/wright1.jpg',
-      alt: 'Descrizione immagine 1',
-    },
-    {
-      src: 'assets/wright3.jpg',
-      alt: 'Descrizione immagine 2',
-    },
-    // Aggiungi altre immagini
-  ];
+  // images = [
+  //   {
+  //     src: 'assets/wright1.jpg',
+  //     alt: 'Descrizione immagine 1',
+  //   },
+  //   {
+  //     src: 'assets/wright3.jpg',
+  //     alt: 'Descrizione immagine 2',
+  //   },
+  //   // Aggiungi altre immagini
+  // ];
 
-  ngOnInit(): void {
-    this.checkMobileView();
-  }
+  // ngOnInit(): void {
+  //   this.checkMobileView();
+  // }
 
-  checkMobileView() {
-    const screenWidth = window.innerWidth;
-    this.isMobileView = screenWidth < 768; // Adatta la larghezza in base alle tue esigenze
-  }
+  // checkMobileView() {
+  //   const screenWidth = window.innerWidth;
+  //   this.isMobileView = screenWidth < 768; // Adatta la larghezza in base alle tue esigenze
+  // }
 
-  handleImagesLoaded() {
-    this.imagesLoaded = true;
-  }
+  // handleImagesLoaded() {
+  //   this.imagesLoaded = true;
+  // }
 
-  showTabs() {
-    this.isHidden = false;
-  }
+  // showTabs() {
+  //   this.isHidden = false;
+  // }
 
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
-    this.checkMobileView(); // Aggiorna la vista mobile durante lo scorrimento
+  // @HostListener('window:scroll', ['$event'])
+  // onWindowScroll() {
+  //   this.checkMobileView(); // Aggiorna la vista mobile durante lo scorrimento
 
-    const scrollPosition = window.scrollY;
-    const triggerPosition = 100;
+  //   const scrollPosition = window.scrollY;
+  //   const triggerPosition = 100;
 
-    if (this.imagesLoaded && scrollPosition > triggerPosition) {
-      this.isSticky = true;
-    } else {
-      this.isSticky = false;
-    }
+  //   if (this.imagesLoaded && scrollPosition > triggerPosition) {
+  //     this.isSticky = true;
+  //   } else {
+  //     this.isSticky = false;
+  //   }
 
-    if (this.imagesLoaded && scrollPosition > triggerPosition) {
-      this.isHidden = scrollPosition > triggerPosition + 50;
-    } else {
-      this.isHidden = false;
-    }
-  }
+  //   if (this.imagesLoaded && scrollPosition > triggerPosition) {
+  //     this.isHidden = scrollPosition > triggerPosition + 50;
+  //   } else {
+  //     this.isHidden = false;
+  //   }
+  // }
 }
