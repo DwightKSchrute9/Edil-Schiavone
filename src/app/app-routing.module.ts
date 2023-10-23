03f709651bb85a4f5eba75ac87001259d16edeae
Silvio Lampi <silvio.lampignano@skyit.it>
2023-10-23 20:50:30 +0200

    switch Header component and Home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Importa il componente HomeComponent
 import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
-import { ServiziComponent } from './servizi/servizi.component'; // Importa le altre componenti necessarie
+import { ServiziComponent } from './servizi/servizi.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent }, // Rotta vuota per la home
   { path: 'chi-siamo', component: ChiSiamoComponent },
   { path: 'servizi', component: ServiziComponent },
   // Aggiungi le altre rotte per le tue sezioni
